9bcc1a3c8d34f181e922f1a76b4f1a650da6323c
nicoclock <nicolas.charpin@oclock.io>
2022-03-15 10:24:56 +0100

    Data json

---
+++ data/got.js
@@ -0,0 +1,261 @@
+module.exports = {
+    families: [
+        { 
+            id: 1,
+            name:'Maison Arryn', 
+            description: 'La maison Arryn est l\'une des grandes maisons des Sept Couronnes et gouverne la région du Val (The Vale). Elle descend des rois de la Montagne et du Val (Kings of Mountain and Vale) et a son siège à la forteresse des Eyrié (The Eyrie) un petit château situé au sommet d\'une montagne. Les chefs de la maison Arryn sont gouverneurs de l\'Est (Warden of the East) depuis la fondation des Sept-Couronnes. Son blason est un faucon blanc sur ciel de lune blanche, et sa devise est « Aussi haute qu\'honneur » (« As High as Honor »). Les bâtards nés au Val sont nommés « Stone ».'
+        },
+        {
+            id: 2,
+            name: 'Maison Baratheon', 
+            description: 'La maison Baratheon est la plus récente des Grandes maisons des Sept Couronnes, et gouverne la région des Terres de l\'Orage (The Stormlands). Fondée par le frère bâtard du premier roi Targaryen, Orys Baratheon, son siège se situe à Accalmie (Storm\'s End) que gère Renly Baratheon, alors que sa branche royale est à la capitale Port-Réal (King\'s Landing) avec Robert Baratheon, et la branche cadette de Stannis Baratheon se tient aussi à Peyredragon (Dragonstone). Son blason est un cerf noir sur champ doré, et sa devise est « Nôtre est la fureur » (« Ours is the Fury »). Les bâtards nés dans les terres de l\'Orage sont nommés « Storm ». Le blason et la devise des Baratheon dérivent de ceux de la défunte maison Durrandon, la lignée royale des « Rois de l\'Orage » qui régnaient sur les Terres de l\'Orage jusqu\'à l\'arrivée des Targaryen, dont Orys Baratheon qui tua le dernier Roi de l\'Orage et épousa sa fille, faisant du blason et de la devise des Durrandon ses propriétés.'
+        },
+        {
+            id: 3,
+            name: 'Maison Greyjoy', 
+            description: 'La maison Greyjoy est l\'une des grandes familles de Westeros et la suzeraine des Îles de Fer (The Iron Islands) et a son siège à Pyk (Pyke). Son blason est un kraken doré sur champ noir et sa devise est « Nous ne semons pas » (« We Do Not Sow »). Les Greyjoy devinrent les Seigneurs protecteurs des Îles de Fer (Lords Paramount of the Iron Islands) après que la maison Targaryen a conquis les Sept Couronnes et permis aux Fer-nés (Ironborn) de choisir leur dirigeant. Les Greyjoy sont réputés pour la puissance de leur flotte, et ils ont souvent conduits des raids contre d\'autres maisons majeures, dans un but de pillage. Les bâtards nés dans les Îles de Fer sont nommés « Pyke ».'
+        },
+        {
+            id: 4,
+            name: 'Maison Lannister', 
+            description: 'La maison Lannister est la maison suzeraine des Terres de l\'Ouest et a son siège à Castral Roc. Son blason est un lion d\'or rugissant sur champ rouge et sa devise est « Je rugis » (« Hear Me Roar ») bien que la devise officieuse des Lannister soit « Un Lannister paie toujours ses dettes ». Les chefs de la maison Lannister sont gouverneurs de l\'Ouest (Warden of the West) depuis la fondation des Sept-Couronnes. C\'est la maison la plus riche des Sept Couronnes car elle contrôle de nombreuses mines d\'or. Les Lannister sont généralement blonds aux yeux verts, un aspect courant parmi les membres de la maison, notamment les enfants de la branche aînée de la maison.'
+        },
+        {
+            id: 5,
+            name: 'Maison Martell', 
+            description: 'La maison Martell est la maison suzeraine de la péninsule de Dorne et a son siège à Lancehélion. Son blason est un soleil rouge traversé d\'une lance dorée sur champ orange et sa devise est « Insoumis, invaincus, intacts » (« Unbowed, Unbent, Unbroken »). Les bâtards nés à Dorne sont nommés « Sand ». Le peuple de Dorne, les Dorniens, est issu d\'un métissage entre les Andals qui habitaient le territoire avant la Conquête et le peuple Rhoynar, venant d\'Essos et fuyant l\'expansion de Valyria, Andals et Rhoynar se mélangèrent et le peuple Dornien garda plusieurs des coutumes des Rhoynars, notamment le fait que la succession ne se fait pas par primogéniture mâle, c\'est-à-dire que le plus proche parent, fils ou fille, hérite du parent décédé. Contrairement aux autres provinces du Royaume des Sept Couronnes, Dorne est considérée comme une Principauté, et son seigneur n\'est pas un Seigneur Suzerain mais un Prince ou une Princesse.'
+        },
+        {
+            id: 6,
+            name: 'Maison Stark', 
+            description: 'La maison Stark est la maison suzeraine du Nord et son siège se situe à Winterfell. Leur blason est un « loup-garou » gris sur champ blanc, et leur devise est « L\'hiver vient » (« Winter is Coming »). Les Stark descendent de Bran le Bâtisseur qui, selon la légende, a bâti Winterfell et le Mur avec l\'aide des enfants de la forêt, un peuple disparu adepte de la magie. Puisque Bran est censé avoir vécu il y a 8 000 ans, cela fait des Stark l\'une des plus vieilles familles de Westeros. Ils ont été rois du Nord pendant presque toute cette période, jusqu\'à ce que le dernier roi Torrhen se soumette à Aegon le Conquérant. Les chefs de la maison Stark sont gouverneurs du Nord (Warden of the North) depuis. Comme la plupart des Maisons du Nord, à l\'exception des Manderly, les Stark descendent d\'une lignée préservée des Premiers Hommes. Comme pour les autres familles nobles du Nord, les batârds issus de la famille Stark sont nommés « Snow ».'
+        },
+        {
+            id: 7,
+            name: 'Maison Targaryen', 
+            description: 'La maison Targaryen est une famille descendante du puissant empire de Valyria, à Essos, ayant conquis les Sept Couronnes avec trois gigantesques dragons, montés par Aegon le Conquérant et ses deux sœurs, Rhaennys et Visenya. S\'ensuivirent presque trois siècles de règne de la dynastie Targaryen à Port-Réal, qui s\'achevèrent lorsque Robert Baratheon détrôna Aerys II le Dément. Son blason est un dragon tricéphale rouge sur champ noir et sa devise est « Feu et Sang » (« Fire and Blood »).'
+        },
+        {
+            id: 8,
+            name: 'Maison Tully', 
+            description: 'La maison Tully est la maison suzeraine de la région du Conflans et a son siège à Vivesaigues. Son blason est une truite argentée sur champ bleu et rouge et sa devise est « Famille, Devoir, Honneur » (« Family, Duty, Honor »).'
+        },
+        {
+            id: 9,
+            name: 'Maison Tyrell', 
+            description: 'La maison Tyrell est l\'une des grandes maisons des Sept Couronnes et la maison suzeraine de la région du Bief (Reach). Elle a son siège à Hautjardin (Highgarden). Son blason est une rose d\'or sur champ vert et sa devise est « Plus haut, plus fort » (« Growing Strong »). Les Tyrell devinrent les seigneurs du Bief après que le roi du Bief fut tué et que la maison royale des Targaryen eut établi les Tyrell, alors intendants du roi du Bief, seigneurs de Hautjardin. Leur arrivée au pouvoir est cependant contestée par plusieurs maisons plus puissantes, notamment la maison Florent. Les chefs de la maison Tyrell sont gouverneurs du Sud (Warden of the South) depuis. Les femmes de la maison Tyrell sont réputées pour être des leaders perspicaces et habiles. Les bâtards nés dans le Bief sont appelés « Flowers ».'
+        }
+    ],
+    members: [
+        {
+            id: 1,
+            name: 'John Arryn',
+            familyId: 1
+        },
+        {
+            id: 2,
+            name: 'Lysa Tully',
+            familyId: 8
+        },
+        {
+            id: 3,
+            name: 'Robert Arryn',
+            familyId: 1
+        },
+        {
+            id: 4,
+            name: 'Robert Baratheon',
+            familyId: 2
+        },
+        {
+            id: 5,
+            name: 'Cersei Lannister',
+            familyId: 4
+        },
+        {
+            id: 6,
+            name: 'Stannis Baratheon',
+            familyId: 2
+        },
+        {
+            id: 7,
+            name: 'Renly Baratheon',
+            familyId: 2
+        },
+        {
+            id: 8,
+            name: 'Joffrey Baratheon',
+            familyId: 2
+        },
+        {
+            id: 9,
+            name: 'Margaery Tyrell',
+            familyId: 9
+        },
+        {
+            id: 10,
+            name: 'Myrcella Baratheon',
+            familyId: 2
+        },
+        {
+            id: 11,
+            name: 'Tommen Baratheon',
+            familyId: 2
+        },
+        {
+            id: 12,
+            name: 'Balon Greyjoy',
+            familyId: 3
+        },
+        {
+            id: 13,
+            name: 'Euron Greyjoy',
+            familyId: 3
+        },
+        {
+            id: 14,
+            name: 'Theon Greyjoy',
+            familyId: 3
+        },
+        {
+            id: 15,
+            name: 'Asha Greyjoy',
+            familyId: 3
+        },
+        {
+            id: 16,
+            name: 'Tywin Lannister',
+            familyId: 4
+        },
+        {
+            id: 17,
+            name: 'Kevan Lannister',
+            familyId: 4
+        },
+        {
+            id: 18,
+            name: 'Jaime Lannister',
+            familyId: 4
+        },
+        {
+            id: 19,
+            name: 'Tyrion Lannister',
+            familyId: 4
+        },
+        {
+            id: 20,
+            name: 'Sansa Stark',
+            familyId: 6
+        },
+        {
+            id: 21,
+            name: 'Doran Martell',
+            familyId: 5
+        },
+        {
+            id: 22,
+            name: 'Oberyn Martell',
+            familyId: 5
+        },
+        {
+            id: 23,
+            name: 'Elia Martell',
+            familyId: 5
+        },
+        {
+            id: 24,
+            name: 'Arianne Martell',
+            familyId: 5
+        },
+        {
+            id: 25,
+            name: 'Eddard Stark',
+            familyId: 6
+        },
+        {
+            id: 26,
+            name: 'Catelyn Tully',
+            familyId: 8
+        },
+        {
+            id: 27,
+            name: 'Lyanna Stark',
+            familyId: 6
+        },
+        {
+            id: 28,
+            name: 'Robb Stark',
+            familyId: 6
+        },
+        {
+            id: 29,
+            name: 'Arya Stark',
+            familyId: 6
+        },
+        {
+            id: 30,
+            name: 'Bran Stark',
+            familyId: 6
+        },
+        {
+            id: 31,
+            name: 'Rickon Stark',
+            familyId: 6
+        },
+        {
+            id: 32,
+            name: 'Aery II Targaryen',
+            familyId: 7
+        },
+        {
+            id: 33,
+            name: 'Rhaegar Targaryen',
+            familyId: 7
+        },
+        {
+            id: 34,
+            name: 'Viseris Targaryen',
+            familyId: 7
+        },
+        {
+            id: 35,
+            name: 'Daenerys Targaryen',
+            familyId: 7
+        },
+        {
+            id: 36,
+            name: 'John Snow/Targaryen',
+            familyId: 7
+        },
+        {
+            id: 37,
+            name: 'Hoster Tully',
+            familyId: 8
+        },
+        {
+            id: 38,
+            name: 'Brynden Tully',
+            familyId: 8
+        },
+        {
+            id: 39,
+            name: 'Edmure Tully',
+            familyId: 8
+        },
+        {
+            id: 40,
+            name: 'Olenna Tyrell',
+            familyId: 9
+        },
+        {
+            id: 41,
+            name: 'Mace Tyrell',
+            familyId: 9
+        },
+        {
+            id: 42,
+            name: 'Loras Tyrell',
+            familyId: 9
+        }
+    ]
+};
